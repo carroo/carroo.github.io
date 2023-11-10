5a096578aa1ea508dd2d5a095fbd32d118581db2
carroo <caturosamma@gmail.com>
2023-11-10 22:33:59 +0700

    6th ver

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,29 @@ module.exports = {
                 'serif': ['Georgia', 'Cambria', 'serif'],
                 'mono': ['Monaco', 'Courier New', 'monospace'],
             },
+            animation: {
+                'lentur': 'lentur 16s linear infinite'
+            },
+            keyframes: {
+                lentur: {
+                    '0%, 100%': {
+                        borderRadius: '63% 37% 73% 27% / 37% 67% 33% 63%',
+                        backgroundColor: '#52616B'
+                    },
+                    '25%': {
+                        borderRadius: '26% 74% 49% 51% / 35% 53% 47% 65%',
+                        backgroundColor: '#707E87'
+                    },
+                    '50%': {
+                        borderRadius: '64% 36% 62% 38% / 55% 32% 68% 45%',
+                        backgroundColor: '#7F8990'
+                    },
+                    '75%': {
+                        borderRadius: '30% 70% 70% 30% / 30% 30% 70% 70% ',
+                        backgroundColor: '#707E87',
+                    },
+                },
+            }
         },
     },
     plugins: [],
